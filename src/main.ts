e38ef90c3b850fb8b26459591fa13ce8c7084586
azukiazusa1 <cycleippei@gmail.com>
2025-09-13 11:41:03 +0900

    add subtract function

--- src/main.ts
+++ src/main.ts
@@ -2,3 +2,8 @@ export function add(a: number, b: number): number {
   return a + b;
 }
 console.log(add(2, 3));
+
+export function subtract(a: any, b: any): number {
+  return a - b;
+}
+console.log(subtract(5, 2));
